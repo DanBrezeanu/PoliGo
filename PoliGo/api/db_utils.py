13cb83c3da5b6d2d6974b6436f4956f6de581501
Dan Brezeanu <dan.brezeanu@nxp.com>
2020-02-21 17:31:35 +0200

    Added Docstrings
    Signed-off-by: Dan Brezeanu <dan.brezeanu@nxp.com>

--- PoliGo/api/db_utils.py
+++ PoliGo/api/db_utils.py
@@ -5,24 +5,37 @@ import json
 import store
 
 def check_stock(request):
+    """
+    Queries the database for the given GET request parameters.
+
+    :param request: request from view function
+    :type request: HttpRequest
+
+    :returns: JSON containing the requested products or Error 422 if a requested product does not exist
+    :rtype: str
+    """
+
     get_options = dict(request.GET)
 
-
+    # No other parameters except API-KEY were provided -> return all products
     if len(get_options) == 1:
         return json.dumps([ProductSerializer(query).data for query in Product.objects.all()])
     
     queried_products = set()
-
     try:
+        # Find products with the requested SKUs
         if 'SKU' in get_options:
             queried_products.update([Product.objects.get(SKU = sku) for sku in get_options['SKU']])
 
+        # Find products with the requested names
         if 'name' in get_options:
             queried_products.update([Product.objects.get(name = name) for name in get_options['name']])
         
+        # Find products with the requested categories
         if 'category' in get_options:
             queried_products.update([Product.objects.get(category = category) for category in get_options['category']])
         
+        # Find products with the requested stocks
         if 'stock' in get_options:
             queried_products.update([Product.objects.get(stock = stock) for stock in get_options['stock']])
     except store.models.Product.DoesNotExist:
--- PoliGo/api/views.py
+++ PoliGo/api/views.py
@@ -15,27 +15,52 @@ def Home(request):
     return HttpResponse('ok')
 
 def key_to_user(request):
-    try:
+    """
+    Returns Profile associated to the api_key provided in request.
+
+    :param request: request from view function
+    :type request: HttpRequest
+
+    :returns: Profile with api_key equal to the one provided or None if not found
+    :rtype: Profile/None
+    """
+    
+    # Get API-KEY from request
+    try:    
         api_key = request.GET['api_key']
-    except:
+    except: # No API-KEY provided
         return None
 
-    try:
+    # Find profile matching the API-KEY
+    try:    
         return Profile.objects.get(api_key = api_key)
     except store.models.Profile.DoesNotExist:
         return None
 
         
 def check_stock(request):
+    """
+    Provides and ENDPOINT for querying the products.
+
+    :param request: implicit request
+    :type request: HttpRequest
+
+    :returns: JSON containing the queried products or Error
+    :rtype: HttpResponse
+    """
+
+    # Get profile
     profile = key_to_user(request)
     
+    # Acces Denied
     if profile is None or not profile.user.is_staff:
         return HttpResponse(json.dumps({'error': 403}))
 
+    # Get the queried products
     if request.method == 'GET':
         products_json = db_utils.check_stock(request)
     else:
-        return HttpResponse({'error': 403})
+        return HttpResponse(json.dumps({'error': 503}))
 
     return HttpResponse(products_json)
 
